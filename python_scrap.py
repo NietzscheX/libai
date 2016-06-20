23dba0b3db9f03382025a176b19f252fca0db168
NietzscheX <hellocpp@163.com>
2016-06-20 22:20:28 +0800

    Update python_scrap.py

--- python_scrap.py
+++ python_scrap.py
@@ -52,3 +52,21 @@ if __name__ == '__main__':
     result=check_url("http://cq.122.gov.cn")
     for r in result:
         print(r)
+
+开启调试模式之后发现了问题（在__exception__这里爆出来的）：
+BeautifulSoup = {type} <class 'bs4.BeautifulSoup'>
+HTTPError = {type} <class 'urllib.error.HTTPError'>
+__builtins__ = {module} <module 'builtins' (built-in)>
+__doc__ = {NoneType} None
+__exception__ = {tuple} (<class 'UnicodeEncodeError'>, UnicodeEncodeError('gbk', '<a class="headerlink" href="#what-s-new-in-python-3-0" title="Permalink to this headline">¶</a>', 90, 91, 'illegal multibyte sequence'), None)
+__file__ = {str} 'E:/Pure_python/scrap.py'
+__loader__ = {SourceFileLoader} <_frozen_importlib_external.SourceFileLoader object at 0x00635B90>
+__name__ = {str} '__main__'
+__package__ = {NoneType} None
+__spec__ = {NoneType} None
+check_url = {function} <function check_url at 0x02E41D20>
+r = {Tag} <a class="headerlink" href="#what-s-new-in-python-3-0" title="Permalink to this headline">¶</a>
+result = {ResultSet} [<a accesskey="I" href="../genindex.html" title="General Index">index</a>, <a accesskey="M" href="../modindex.html" title="Global Module Index">modules</a>, <a accesskey="N" href="2.7.html" title="What’s New in Python 2.7">next</a>, <a accesskey="P" href="
+urlopen = {function} <function urlopen at 0x02D98DF8>
+
+
